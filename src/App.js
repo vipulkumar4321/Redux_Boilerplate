80dd12a47cb2c08ea03529b8e76f66f9d06a20f6
Vipul Kumar <vipulkumar@gmail.com>
2024-08-06 15:17:20 +0530

    Redux with Middleware

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./Redux/store";
+import { store } from "./Redux/store";
 import CounterComponent from "./Components/CounterComponent";
 import FlagComponent from "./Components/FlagComponent";
 
 const App = () => (
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <CounterComponent />
-      <FlagComponent />
-    </PersistGate>
+    <CounterComponent />
+    <FlagComponent />
   </Provider>
 );
 export default App;
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,17 +1,9 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
 import { thunk } from "redux-thunk";
 import loggerMiddleware from "./Middleware/loggerMiddleware";
-import persistConfig from "./Persist/persistConfig";
 import rootReducer from "./Reducers";
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const store = createStore(
-  persistedReducer,
+export const store = createStore(
+  rootReducer,
   applyMiddleware(thunk, loggerMiddleware)
 );
-
-const persistor = persistStore(store);
-
-export { store, persistor };
